1e23b1854635cfd53f0c9c973efebbd9e9f23ee3
Moritz <moritz.wolter@web.de>
2023-11-03 08:59:41 +0100

    Order Cities by best selling
    Signed-off-by: Moritz <moritz.wolter@web.de>

--- SalesAnalysis/analysis.py
+++ SalesAnalysis/analysis.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import matplotlib.pyplot as plt
 all_data = pd.read_csv('./SalesAnalysis/all_month_data.csv')
 all_data.dropna(inplace=True)
 
@@ -10,14 +10,23 @@ all_data['Quantity Ordered'] = pd.to_numeric(all_data['Quantity Ordered'], error
 all_data = all_data[all_data['Month'] != 'Order Date']
 all_data['Month'] = all_data['Month'].astype('int32')
 
+all_data['Value'] = all_data['Quantity Ordered'] * all_data['Price Each']
+all_data['City'] = all_data['Purchase Address'].apply(lambda x: x.split(',')[1] + x.split(',')[2][:3])
+
 
 def get_best_month(all_data: pd.DataFrame):
-    all_data['Value'] = all_data['Quantity Ordered'] * all_data['Price Each']
+    
     months = all_data.groupby('Month', as_index=False)['Value'].sum()
 
     return months[months.Value == max(months.Value)]['Month']
 
+def get_best_city(all_data: pd.DataFrame):
+    
+    best = all_data.groupby('City', as_index=False)['Value'].sum()
+    return best
 
-print(all_data)
+best_city = get_best_city(all_data)
+
+print(best_city)
 
 
